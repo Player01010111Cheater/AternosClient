523c3cb89311b04eedfbfc07d6ecd81028741473
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 12:48:18 +0300

    Update request.lua

--- util/items/request.lua
+++ util/items/request.lua
@@ -1,7 +1,8 @@
-return function(url, method)
+return function(url, method, head)
   local success, result = pcall(request, {
       Url = url,
       Method = method,
+      Headers = head
   })
   return success, result
 end
